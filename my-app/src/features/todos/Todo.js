30cbb0b71aab6d31634fe186475e4c263e45d6b4
Thomas Maynard <90271276+Thomas4897@users.noreply.github.com>
2022-04-22 00:33:59 +0800

    Finishing Frontend

--- my-app/src/features/todos/Todo.js
+++ my-app/src/features/todos/Todo.js
@@ -14,8 +14,6 @@ import {
 
 import { useState } from "react";
 
-// import { addTodo, selectTodoArray } from "../features/todos/todoSlice";
-
 export default function Todo() {
   const todoList = useSelector(selectTodoArray);
   const dispatch = useDispatch();
@@ -30,10 +28,40 @@ export default function Todo() {
   const [description, setDescription] = useState("");
   const [priority, setPriority] = useState("");
   const [filterComplete, setFilterComplete] = useState("All");
+  const [filterPriority, setFilterPriority] = useState("All");
+  const [sortPriority, setSortPriority] = useState(false);
+  const [sortComplete, setSortComplete] = useState(false);
+
+  let prioritySortingOrder = {
+    critical: 1,
+    high: 2,
+    medium: 3,
+    low: 4,
+  };
+
+  let completeSortingOrder = {
+    true: 1,
+    false: 2,
+  };
+
+  const setStatistics = todoList.map((todo) => {
+    todo.isComplete ? (numberComplete += 1) : (numberIncomplete += 1);
+
+    if (todo.priority.toLowerCase() === "critical") {
+      criticalPriorities += 1;
+    } else if (todo.priority.toLowerCase() === "high") {
+      highPriorities += 1;
+    } else if (todo.priority.toLowerCase() === "medium") {
+      mediumPriorities += 1;
+    } else if (todo.priority.toLowerCase() === "low") {
+      lowPriorities += 1;
+    }
+  });
 
   return (
     <div className="Dashboard">
       <div className="TodoList">
+        Filter Status:{" "}
         <select
           className="FilterComplete"
           onChange={(e) => {
@@ -43,144 +71,236 @@ export default function Todo() {
           <option value="All">All</option>
           <option value={true}>Complete</option>
           <option value={false}>Incomplete</option>
+        </select>{" "}
+        Filter Priority:{" "}
+        <select
+          className="FilterPriority"
+          onChange={(e) => {
+            setFilterPriority(e.target.value);
+          }}
+        >
+          <option value="All">All</option>
+          <option value="Critical">Critical</option>
+          <option value="High">High</option>
+          <option value="Medium">Medium</option>
+          <option value="Low">Low</option>
         </select>
-        {todoList.map((todo) => {
-          todo.isComplete ? (numberComplete += 1) : (numberIncomplete += 1);
+        <button
+          onClick={() => {
+            setSortPriority(!sortPriority);
+          }}
+        >
+          Sort Priority
+        </button>
+        <button
+          onClick={() => {
+            setSortComplete(!sortComplete);
+          }}
+        >
+          Sort Complete
+        </button>
+        {todoList
+          .filter((todo) => {
+            if (
+              filterComplete === "All" ||
+              todo.isComplete.toString() === filterComplete
+            ) {
+              if (
+                filterPriority === "All" ||
+                todo.priority === filterPriority
+              ) {
+                return todo;
+              }
+            }
+          })
+          .sort(function (a, b) {
+            if (sortPriority) {
+              if (
+                prioritySortingOrder[a.priority.toLowerCase()] <
+                prioritySortingOrder[b.priority.toLowerCase()]
+              ) {
+                return -1;
+              }
 
-          if (todo.priority.toLowerCase() === "critical") {
-            criticalPriorities += 1;
-          } else if (todo.priority.toLowerCase() === "high") {
-            highPriorities += 1;
-          } else if (todo.priority.toLowerCase() === "medium") {
-            mediumPriorities += 1;
-          } else if (todo.priority.toLowerCase() === "low") {
-            lowPriorities += 1;
-          }
-          return (
-            <div>
-              <div className="Todo">
-                <div
-                  className="isComplete"
-                  style={{
-                    fontWeight: "bold",
-                    color: todo.isComplete ? "green" : "red",
-                  }}
-                >
-                  {todo.isComplete ? (
-                    <div className="completedDate">
-                      Completed: {todo.completeDate}
-                    </div>
-                  ) : (
-                    "Incomplete"
-                  )}
-                </div>
-                <div>{todo.id}</div>
+              if (
+                prioritySortingOrder[a.priority.toLowerCase()] >
+                prioritySortingOrder[b.priority.toLowerCase()]
+              ) {
+                return 1;
+              }
 
-                {todo.editing === true ? (
-                  <div>
-                    Title:{" "}
-                    <input
-                      type="text"
-                      name="title"
-                      placeholder={todo.title}
-                      onChange={(e) => {
-                        setTitle(e.target.value);
-                      }}
-                    />
+              return 0;
+            }
+            if (sortComplete) {
+              console.log("a", completeSortingOrder[a.isComplete]);
+              console.log("b", completeSortingOrder[b.isComplete]);
+
+              if (
+                completeSortingOrder[a.isComplete] <
+                completeSortingOrder[b.isComplete]
+              ) {
+                return -1;
+              }
+
+              if (
+                completeSortingOrder[a.isComplete] >
+                completeSortingOrder[b.isComplete]
+              ) {
+                return 1;
+              }
+
+              return 0;
+            }
+          })
+          // .sort(function (a, b) {
+          //   if (sortComplete) {
+          //     console.log("a", completeSortingOrder[a.isComplete]);
+          //     console.log("b", completeSortingOrder[b.isComplete]);
+
+          //     if (
+          //       completeSortingOrder[a.isComplete] <
+          //       completeSortingOrder[b.isComplete]
+          //     ) {
+          //       return -1;
+          //     }
+
+          //     if (
+          //       completeSortingOrder[a.isComplete] >
+          //       completeSortingOrder[b.isComplete]
+          //     ) {
+          //       return 1;
+          //     }
+
+          //     return 0;
+          //   }
+          // })
+          .map((todo) => {
+            return (
+              <div>
+                <div className="Todo">
+                  <div
+                    className="isComplete"
+                    style={{
+                      fontWeight: "bold",
+                      color: todo.isComplete ? "green" : "red",
+                    }}
+                  >
+                    {todo.isComplete ? (
+                      <div className="completedDate">
+                        Completed: {todo.completeDate}
+                      </div>
+                    ) : (
+                      "Incomplete"
+                    )}
+                  </div>
+                  <div>{todo.id}</div>
+
+                  {todo.editing ? (
                     <div>
-                      Priority:{" "}
-                      <select
-                        style={{
-                          color: priority === "" ? "#666" : "",
-                        }}
-                        onChange={(e) => {
-                          setPriority(e.target.value);
-                        }}
-                      >
-                        <option value="">{todo.priority}</option>
-                        <option value="Critical">Critical</option>
-                        <option value="High">High</option>
-                        <option value="Medium">Medium</option>
-                        <option value="Low">Low</option>
-                      </select>
-                    </div>
-                    <div className="creationDate">
-                      Created: {todo.creationDate}
-                    </div>
-                    <div>
-                      Description:{" "}
+                      Title:{" "}
                       <input
                         type="text"
-                        name="description"
-                        placeholder={todo.description}
+                        name="title"
+                        placeholder={todo.title}
                         onChange={(e) => {
-                          setDescription(e.target.value);
+                          setTitle(e.target.value);
                         }}
                       />
+                      <div>
+                        Priority:{" "}
+                        <select
+                          style={{
+                            color: priority === "" ? "#666" : "",
+                          }}
+                          onChange={(e) => {
+                            setPriority(e.target.value);
+                          }}
+                        >
+                          <option value="">{todo.priority}</option>
+                          <option value="Critical">Critical</option>
+                          <option value="High">High</option>
+                          <option value="Medium">Medium</option>
+                          <option value="Low">Low</option>
+                        </select>
+                      </div>
+                      <div className="creationDate">
+                        Created: {todo.creationDate}
+                      </div>
+                      <div>
+                        Description:{" "}
+                        <input
+                          type="text"
+                          name="description"
+                          placeholder={todo.description}
+                          onChange={(e) => {
+                            setDescription(e.target.value);
+                          }}
+                        />
+                      </div>
                     </div>
-                  </div>
-                ) : (
-                  <div>
-                    <div className="title">Title: {todo.title}</div>
-                    <div className="priority">Priority: {todo.priority}</div>
-                    <div className="creationDate">
-                      Created: {todo.creationDate}
+                  ) : (
+                    <div>
+                      <div className="title">Title: {todo.title}</div>
+                      <div className="priority">Priority: {todo.priority}</div>
+                      <div className="creationDate">
+                        Created: {todo.creationDate}
+                      </div>
+                      <div className="description">
+                        Description: {todo.description}
+                      </div>
                     </div>
-                    <div className="description">
-                      Description: {todo.description}
-                    </div>
-                  </div>
-                )}
-                <button
-                  onClick={() =>
-                    dispatch(
-                      editTodo({
-                        ...todo,
-                        editing: !todo.editing,
-                      })
-                    )
-                  }
-                >
-                  Edit
-                </button>
-                <button
-                  onClick={() => {
-                    if (todo.editing === true) {
+                  )}
+                  <button
+                    onClick={() =>
                       dispatch(
                         editTodo({
                           ...todo,
-                          title: title,
-                          priority: priority,
-                          description: description,
-                          editing: false,
+                          editing: !todo.editing,
                         })
-                      );
+                      )
                     }
-                  }}
-                >
-                  Save
-                </button>
-                <button onClick={() => dispatch(deleteTodo(todo))}>
-                  DELETE TODO
-                </button>
-                <button
-                  onClick={() =>
-                    dispatch(
-                      editTodo({
-                        ...todo,
-                        isComplete: !todo.isComplete,
-                        completeDate: new Date().toISOString(),
-                      })
-                    )
-                  }
-                >
-                  Complete/Incomplete Todo
-                </button>
+                  >
+                    Edit
+                  </button>
+                  <button
+                    onClick={() => {
+                      if (todo.editing === true) {
+                        dispatch(
+                          editTodo({
+                            ...todo,
+                            title: title,
+                            priority: priority,
+                            description: description,
+                            editing: false,
+                          })
+                        );
+                      }
+                    }}
+                  >
+                    Save
+                  </button>
+                  <button onClick={() => dispatch(deleteTodo(todo))}>
+                    DELETE TODO
+                  </button>
+                  <button
+                    onClick={() =>
+                      dispatch(
+                        editTodo({
+                          ...todo,
+                          isComplete: !todo.isComplete,
+                          completeDate: new Date().toISOString(),
+                        })
+                      )
+                    }
+                  >
+                    Complete/Incomplete Todo
+                  </button>
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
       </div>
+
       <div className="TodoStatistics">
         <div className="Statistics">
           <div
